d71fa0c7c5c11e8b671766817223cbf65642c336
unknown <gagik4simonyan@gmail.com>
2021-03-03 21:27:55 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+const Index = () => {
+  return (
+    <div>
+     Index 
+     </div>
+  );
+
+};
+
+export default Index;
+
+ReactDom.render(<Index/> ,document.getElementById("root"))
